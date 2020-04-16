cfa46896d4e2feb8407c04f3ed52585cf542abbf
Subhajit Sahu <wolfram77@gmail.com>
2020-04-16 20:09:48 +0530

    led blinking

--- blink.vhd
+++ blink.vhd
@@ -6,6 +6,7 @@ use ieee.std_logic_unsigned.all;
 
 entity blink is
 port(
+	clk: in std_logic;
 	led: out std_logic_vector(3 downto 0)
 );
 end blink;
@@ -13,5 +14,18 @@ end blink;
 
 architecture bh of blink is
 begin
-	led <= (others => '0`');
+	p1: process(clk)
+	variable cnt: integer range 0 to 48000000;
+	variable ledstatus: std_logic;
+	begin
+		if clk'event and clk = '1' then
+			if cnt < (4800000-1) then
+				cnt := cnt + 1;
+			else
+				cnt := 0;
+				ledstatus := not ledstatus;
+			end if;
+		end if;
+		led <= (others => ledstatus);
+	end process;
 end bh;
