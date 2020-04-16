262ead8aa689e510d100ded3c37ebfa75e64cb2c
Subhajit Sahu <wolfram77@gmail.com>
2020-04-16 20:14:28 +0530

    2 spaces

--- blink.vhd
+++ blink.vhd
@@ -6,26 +6,26 @@ use ieee.std_logic_unsigned.all;
 
 entity blink is
 port(
-	clk: in std_logic;
-	led: out std_logic_vector(3 downto 0)
+  clk: in std_logic;
+  led: out std_logic_vector(3 downto 0)
 );
 end blink;
 
 
 architecture bh of blink is
 begin
-	p1: process(clk)
-	variable cnt: integer range 0 to 48000000;
-	variable ledstatus: std_logic;
-	begin
-		if clk'event and clk = '1' then
-			if cnt < (4800000-1) then
-				cnt := cnt + 1;
-			else
-				cnt := 0;
-				ledstatus := not ledstatus;
-			end if;
-		end if;
-		led <= (others => ledstatus);
-	end process;
+  p1: process(clk)
+  variable cnt: integer range 0 to 48000000;
+  variable ledstatus: std_logic;
+  begin
+    if clk'event and clk = '1' then
+      if cnt < (4800000-1) then
+        cnt := cnt + 1;
+      else
+        cnt := 0;
+        ledstatus := not ledstatus;
+      end if;
+    end if;
+    led <= (others => ledstatus);
+  end process;
 end bh;
